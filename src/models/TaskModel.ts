0644daf4ac2ba65a52da74c723ad9e41ac19cbb3
Walter Gotschalg <walter.gms@gmail.com>
2026-02-18 18:48:45 -0300

    teste

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,11 @@
+import type { TaskType } from './TaskType';
+
+export type TaskModel = {
+  id: string;
+  name: string;
+  duration: number;
+  startDate: number;
+  completeDate: number | null;
+  interruptDate: number | null;
+  type: TaskType;
+};
